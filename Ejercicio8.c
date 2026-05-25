4c0e83f2c41a5f4d8d88ec2a3431a9bfe5ee62e6
Yaxché <yaxche65@gmail.com>
2026-05-24 20:37:26 -0600

    Ejercicio8.c

---
+++ Ejercicio8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Ingrese dos numeros enteros: ");
+    scanf("%d %d", &num1, &num2);
+    
+    if (num1 % num2 == 0) {
+        printf("%d es divisor de %d\n", num2, num1);
+    } else if (num2 % num1 == 0) {
+        printf("%d es divisor de %d\n", num1, num2);
+    } else {
+        printf("Ninguno es divisor del otro.\n");
+    }
+    return 0;
+}
