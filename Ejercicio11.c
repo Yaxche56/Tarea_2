84150de1a514a0a3831558a720114d0848639d9d
Yaxché <yaxche65@gmail.com>
2026-05-24 20:39:04 -0600

    Ejercicio11.c

---
+++ Ejercicio11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int suma = 0;
+    
+    for (int i = 10; i <= 50; i += 2) {
+        suma += i;
+    }
+    
+    printf("La suma de los numeros pares entre 10 y 50 es: %d\n", suma);
+    return 0;
+}
