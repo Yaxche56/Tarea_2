73aec37703db7bda073037c0bd6d57b05c2f3c60
Yaxché <yaxche65@gmail.com>
2026-05-24 20:39:40 -0600

    Ejercicio12.c

---
+++ Ejercicio12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    long long producto = 1;
+    
+    printf("Ingrese N: ");
+    scanf("%d", &N);
+    
+    for (int i = 1; i <= N; i++) {
+        producto *= i;
+    }
+    
+    printf("El producto de los primeros %d numeros es: %lld\n", N, producto);
+    return 0;
+}
