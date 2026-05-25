b9699e57a0942efc6a9de2fa55f418c4da871b66
Yaxché <yaxche65@gmail.com>
2026-05-24 20:35:20 -0600

    Ejercicio3.c

---
+++ Ejercicio3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float dolares, pesos;
+    printf("Ingrese la cantidad en dolares: ");
+    scanf("%f", &dolares);
+    
+    pesos = dolares * 11.96;
+    printf("Equivalente en pesos: %.2f\n", pesos);
+    return 0;
+}
