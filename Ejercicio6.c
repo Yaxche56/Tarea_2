41c1688b67dffc08f3637f081a793e2ff8aaf64a
Yaxché <yaxche65@gmail.com>
2026-05-24 20:36:37 -0600

    Ejercicio6.c

---
+++ Ejercicio6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float sueldo;
+    printf("Ingrese el sueldo del trabajador: ");
+    scanf("%f", &sueldo);
+    
+    if (sueldo < 1000) {
+        sueldo = sueldo * 1.15;
+    } else {
+        sueldo = sueldo * 1.12;
+    }
+    
+    printf("Sueldo con aumento: $%.2f\n", sueldo);
+    return 0;
+}
